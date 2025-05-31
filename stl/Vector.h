93f9dd5979cc77157fb94443a13196c75dd513a4
Khoi Nguyen <minhkhoi906@gmail.com>
2025-05-31 11:34:21 +0700

    Overridden vector implementation

--- stl/Vector.h
+++ stl/Vector.h
@@ -1,120 +1,135 @@
 #pragma once
 
-#include <algorithm>
-#include <initializer_list>
+#include <stdexcept>
 #include <utility>
 
-template <typename T> struct Vector {
-    Vector() : m_data(nullptr), m_size(0), m_capacity(0) {}
-    explicit Vector(size_t size)
-        : m_data(new T[size]), m_size(size), m_capacity(size)
+template <typename Type> struct Vector {
+    // Default constructor
+    Vector() noexcept : m_data(nullptr), m_size(0), m_capacity(0){};
+
+    // Parameterized constructor
+    explicit Vector(size_t size) : m_size(size), m_capacity(size)
     {
-    }
-
-    Vector(std::initializer_list<T> list)
-        : m_data(new T[list.size()]), m_size(list.size()), m_capacity(list.size())
-    {
-        std::copy(list.begin(), list.end(), m_data);
-    }
-
-    // Copy ctor
-    Vector(const Vector &other)
-        : m_data(new T[other.m_capacity]), m_size(other.m_size), m_capacity(other.m_capacity) {
-        std::copy(other.m_data, other.m_data + other.m_size, m_data);
-    }
-
-    // Copy assignment
-    Vector &operator=(const Vector &other)
-    {
-        if (this != &other) {
-            T *new_data = new T[other.m_capacity];
-            std::copy(other.m_data, other.m_data + other.m_size, new_data);
-
-            delete[] m_data;
-            m_data = new_data;
-            m_size = other.m_size;
-            m_capacity = other.m_capacity;
+        m_data = new Type[m_capacity];
+        for (size_t i = 0; i < m_size; ++i) {
+            m_data[i] = Type(); // Default initialize elements
         }
-
-        return *this;
     }
 
-    // Move ctor
+    // Copy constructor
+    Vector(const Vector &other) : m_size(other.m_size), m_capacity(other.m_capacity)
+    {
+        m_data = new Type[m_capacity];
+        for (size_t i = 0; i < m_size; ++i) {
+            m_data[i] = other.m_data[i]; // Deep copy elements
+        }
+    }
+
+    // Move constructor
     Vector(Vector &&other)
         : m_data(other.m_data), m_size(other.m_size), m_capacity(other.m_capacity)
     {
-        other.m_data = nullptr;
+        other.m_data = nullptr; // Reset moved-from object
         other.m_size = 0;
         other.m_capacity = 0;
     }
 
-    // Move assignment
+    // Destructor
+    ~Vector()
+    {
+        delete[] m_data;
+        m_data = nullptr; // Avoid dangling pointer
+        m_size = 0;
+        m_capacity = 0; // Reset size and capacity
+    }
+
+    // Copy assignment operator
+    Vector &operator=(const Vector &other)
+    {
+        if (this != &other) {
+            Vector temp(other);
+            std::swap(temp, *this);
+        }
+        return *this;
+    }
+
+    // Move assignment operator
     Vector &operator=(Vector &&other)
     {
         if (this != &other) {
             delete[] m_data;
-
             m_data = other.m_data;
             m_size = other.m_size;
             m_capacity = other.m_capacity;
-
-            other.m_data = nullptr;
+            other.m_data = nullptr; // Reset moved-from object
             other.m_size = 0;
             other.m_capacity = 0;
         }
-
         return *this;
     }
 
-    ~Vector() { delete[] m_data; }
-
     void reserve(size_t new_capacity)
     {
-        if (new_capacity <= m_capacity) {
-            return;
+        if (new_capacity > m_capacity) {
+            Type *new_data = new Type[new_capacity];
+            for (size_t i = 0; i < m_size; ++i) {
+                new_data[i] = std::move(m_data[i]); // Move elements to new storage
+            }
+            delete[] m_data;           // Free old storage
+            m_data = new_data;         // Point to new storage
+            m_capacity = new_capacity; // Update capacity
         }
-
-        T *new_data = new T[new_capacity];
-        std::move(m_data, m_data + m_size, new_data);
-
-        delete[] m_data;
-        m_data = new_data;
-        m_capacity = new_capacity;
     }
 
-    bool empty() const noexcept { return (m_size == 0); }
-    size_t capacity() const noexcept { return m_capacity; }
-    size_t size() const noexcept { return m_size; }
-
-    void push_back(const T &val)
+    void push_back(const Type &value)
     {
         ensure_capacity();
-        m_data[m_size++] = std::move(val);
+        m_data[m_size++] = value;
     }
 
-    void push_back(T &&val)
+    void push_back(Type &&value)
     {
         ensure_capacity();
-        m_data[m_size++] = std::move(val);
+        m_data[m_size++] = std::move(value); // Move new element
     }
 
-    T &operator[](int index)
+    template <typename... Args> void emplace_back(Args &&...args)
     {
-        if (index >= m_size)
+        ensure_capacity();
+        m_data[m_size++] = Type(std::forward<Args>(args)...);
+    }
+
+    bool empty() const
+    {
+        return m_size == 0; // Check if vector is empty
+    }
+
+    size_t size() const
+    {
+        return m_size; // Return current size
+    }
+
+    size_t capacity() const
+    {
+        return m_capacity; // Return current capacity
+    }
+    // Access operator
+    Type &operator[](size_t index)
+    {
+        if (index < 0 || index >= m_size) {
             throw std::out_of_range("Index out of bounds");
-
+        }
         return m_data[index];
     }
 
   private:
-    T *m_data;
+    Type *m_data;
     size_t m_size;
     size_t m_capacity;
 
-    // Ensure there is enough capacity
     void ensure_capacity()
     {
-        if (m_size == m_capacity) {
+        if (m_size >= m_capacity) {
             reserve(m_capacity == 0 ? 1 : m_capacity * 2);
         }
     }
