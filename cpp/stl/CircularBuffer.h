046516c392632ba441a785b4b097c6ff95b37ad3
Khoi Nguyen <minhkhoi906@gmail.com>
2025-12-24 16:44:24 +0700

    feat: add ring buffer

--- cpp/stl/CircularBuffer.h
+++ cpp/stl/CircularBuffer.h
@@ -1,13 +1,62 @@
 #pragma once
 
-#include <Array>
+#include <cstddef>
+#include <stdexcept>
+#include <vector>
 
-template <typename T, size_t S>
-struct CircularBuffer
-{
+struct CircularBuffer {
     CircularBuffer() = default;
-    CircularBuffer(size_t size = S) : {}
+    explicit CircularBuffer(size_t capacity)
+        : m_buffer(capacity), m_capacity(capacity), m_size(0), m_head(0), m_tail(0)
+    {
+        if (capacity == 0) {
+            std::__throw_invalid_argument("Capacity must be > 0");
+        }
+    };
 
-private:
-    std::array<T, S> m_capability;
+    // Functionalities
+    size_t size() const { return m_size; }
+    size_t capacity() const { return m_capacity; }
+    bool isEmpty() const { return m_size == 0; }
+    bool isFull() const { return m_size == m_capacity; }
+
+    // Push
+    void enqueue(int val)
+    {
+        if (isFull()) {
+            m_head = (++m_head) % m_capacity;
+        } else {
+            ++m_size;
+        }
+
+        m_buffer[m_tail] = val;
+        m_tail = (++m_tail) % m_capacity;
+        ;
+    }
+
+    // dequeue
+    int dequeue()
+    {
+        if (isEmpty()) {
+            std::__throw_out_of_range("Queue is empty.");
+        }
+
+        int res = m_buffer[m_head];
+
+        ++m_head;
+        if (m_head == m_capacity) {
+            m_head -= m_capacity;
+        }
+
+        --m_size;
+
+        return res;
+    }
+
+  private:
+    std::vector<int> m_buffer;
+    size_t m_capacity{0};
+    size_t m_size{0};
+    size_t m_head{0}; // index of oldest element
+    size_t m_tail{0}; // index where next enqueue writes
 };
---
+++ cpp/unittests/TestCirCularBuffer.cpp
@@ -0,0 +1,42 @@
+#include "../stl/CircularBuffer.h"
+
+#include <gtest/gtest.h>
+
+TEST(CircularBufferTest, ParameterizedConstructor)
+{
+    CircularBuffer buffer(4);
+
+    EXPECT_EQ(buffer.capacity(), 4);
+    EXPECT_EQ(buffer.size(), 0);
+    EXPECT_TRUE(buffer.isEmpty());
+}
+
+TEST(CircularBufferTest, Enqueue)
+{
+    CircularBuffer buffer(4);
+
+    buffer.enqueue(1);
+    buffer.enqueue(2);
+
+    EXPECT_EQ(buffer.size(), 2);
+    EXPECT_FALSE(buffer.isEmpty());
+}
+
+TEST(CircularBufferTest, Dequeue)
+{
+    CircularBuffer buffer(4);
+
+    buffer.enqueue(1);
+    buffer.enqueue(2);
+
+    EXPECT_EQ(buffer.size(), 2);
+    EXPECT_FALSE(buffer.isEmpty());
+
+    int head = buffer.dequeue();
+    int EXPECTED_VAL = 1;
+
+    EXPECT_EQ(head, EXPECTED_VAL);
+    EXPECT_EQ(buffer.size(), 1);
+}
+
+
